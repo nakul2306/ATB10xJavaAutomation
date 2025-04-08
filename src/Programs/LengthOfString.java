f094da017c3af3e4f4b5ba5ce5d6b5e412c940c1
NakulPusadkar <nakulpusadkar2306@gmail.com>
2025-04-08 18:02:55 +0530

    5 April 2025

---
+++ src/Programs/LengthOfString.java
@@ -0,0 +1,9 @@
+package Programs;
+
+public class LengthOfString {
+    public static void main(String[] args) {
+        String s = " I am Happy";
+        System.out.println("length of String is " + s.length());
+    }
+}
+
